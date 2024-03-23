3dc1dd98c514943ad69cbebd3e800fcf2b51be58
JuanLoncharich <juanandresloncha@gmail.com>
2024-03-23 19:33:08 -0300

    Actualizo insert
    Funciona en ciertos casos, no en todos

--- practicas/tp-arboles-avl/avltree.py
+++ practicas/tp-arboles-avl/avltree.py
@@ -18,43 +18,47 @@ def _print_tree(node, level):
     print('  ' * level + str(node.key))
     _print_tree(node.leftnode, level + 1)
 
+##Realiza una rotación a la derecha de un árbol binario
 
 def rotateRight(tree,node):
-    if node.parent is not None:
-        node.leftnode.parent = node.parent
-        node.parent.rightnode = node.leftnode
-        node.parent = node.leftnode
-    else:
-        tree.root = node.leftnode
-    if node.leftnode.rightnode is None:
-        node.leftnode.rightnode = node
-        node.leftnode = None
-    else:
-        nodeAux = node.leftnode.rightnode
-        node.leftnode.rightnode = node
-        node.leftnode.parent = None
-        node.leftnode = nodeAux
-        nodeAux.parent = node
-    return node.parent
+  if node.parent is not None:
+      node.leftnode.parent = node.parent
+      node.parent.leftnode = node.leftnode
+      node.parent = node.leftnode
+  else:
+      tree.root = node.leftnode
+  if node.leftnode.rightnode is None:
+      node.leftnode.rightnode = node
+      node.leftnode = None
+  else:
+      nodeAux = node.leftnode.rightnode
+      node.leftnode.rightnode = node
+      node.leftnode.parent = None
+      node.leftnode = nodeAux
+      nodeAux.parent = node
+  return node.parent
+            
+##Realiza una rotación a la izquierda de un árbol binario
             
 def rotateLeft(tree,node):
-    if node.parent is not None:
-        node.rightnode.parent = node.parent
-        node.parent.leftnode = node.rightnode
-        node.parent = node.rightnode
-    else:
-        tree.root = node.rightnode
-    if node.rightnode.leftnode is None:
-        node.rightnode.leftnode = node
-        node.rightnode = None
-    else:
-        nodeAux = node.rightnode.leftnode
-        node.rightnode.leftnode = node
-        node.rightnode.parent = None
-        node.rightnode = nodeAux
-        nodeAux.parent = node
-    return node.parent
+  if node.parent is not None:
+      node.rightnode.parent = node.parent
+      node.parent.rightnode = node.rightnode
+      node.parent = node.rightnode
+  else:
+      tree.root = node.rightnode
+  if node.rightnode.leftnode is None:
+      node.rightnode.leftnode = node
+      node.rightnode = None
+  else:
+      nodeAux = node.rightnode.leftnode
+      node.rightnode.leftnode = node
+      node.rightnode.parent = None
+      node.rightnode = nodeAux
+      nodeAux.parent = node
+  return node.parent
 
+##Calcula el balance factor de todos los nodos del árbol
 
 def calculateBalance(tree):
     return calculateBalanceR(tree.root)
@@ -65,6 +69,8 @@ def calculateBalanceR(node):
     node.bf = altura(node.leftnode) - altura(node.rightnode)
     calculateBalanceR(node.leftnode)
     calculateBalanceR(node.rightnode)
+
+##Rebalancea el árbol de manera que quede balanceado en altura
 
 def reBalance(tree):
   return reBalanceR(tree.root,tree)
@@ -92,28 +98,35 @@ def reBalanceS(node,tree):
       rotateLeft(tree,node.leftnode)
     rotateRight(tree,node)
          
+##Calcular la altura de un arbol
 
 def altura(node):
     if node is None:
-        return 0
-    
-    izquierda = altura(node.leftnode)
-    derecha = altura(node.rightnode)
-
-    if izquierda >= derecha:
-        return izquierda + 1
+      return 0
     else:
-        return derecha + 1
+      izquierda = altura(node.leftnode)
+      derecha = altura(node.rightnode)
+      
+      if node.leftnode is None and node.rightnode is None:
+        return 1
+
+      return max(izquierda,derecha) + 1
+  
+  
+##Insertar nodos dentro de un AVL manteniendo su condicion de AVL
 
 def insert(b, element, key):
   if b.root is None:
+    
     node = AVLNode()
     node.bf = 0
     node.key = key
     node.value = element
     b.root = node
     return
+  
   else:
+    
     node = insertR(b.root, element, key)
     while node is not None:
       calculateBalanceBranch(node)
@@ -150,8 +163,6 @@ def insertR(node, element, key):
       else:
         return insertR(node.rightnode,element,key)
       
-      
-      
 ##Complemento de la funcion insert()
 
 def calculateBalanceBranch(node):
@@ -179,27 +190,7 @@ insert(tree,' ', 20)
 insert(tree,' ', 24)
 insert(tree,' ', 26)
 insert(tree,' ', 28)
-#insert(tree,' ', 30)
+insert(tree,' ', 30)
 
 print_tree(tree)
-
-
-#print('___________')
-#calculateBalance(tree)
-
-#reBalance(tree)
-
-#print_tree(tree)
-
-#print('__________________')
-
-#rotateRight(tree,tree.root)
-
-#print_tree(tree)
-
-#print('__________________')
-
-#rotateLeft(tree,tree.root.leftnode)
-
-#print_tree(tree)
 
