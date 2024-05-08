260140e21830253dfd14d5e50868c9e1e0f50763
JuanLoncharich <juanandresloncha@gmail.com>
2024-05-08 09:54:51 -0300

    exercise 5, 6, 7, 8, 9; Implementing open addressing hash

--- practicas/tp-hash/code/dictionary.py
+++ practicas/tp-hash/code/dictionary.py
@@ -1,26 +1,20 @@
-import random
 import math
 
-def hashFunction(k,m):
+def hashFunction(k, m):
     hash_value = 0
     k = str(k)
-    if len(k) == 1:
-        random.seed(ord(k))
-        for i in range(1,ord(k)):
-            hash_value = (hash_value + ord(k) * random.randint(2, 1000))
-        return hash_value % m
-    else:
-        for i in range(1,len(k)):
-            random.seed(ord(k[i]))
-            for j in range(0,ord(k[i])):
-                hash_value = (hash_value + ord(k[i]) * random.randint(2, 1000)) % m
-        return hash_value
+    a = 17921
+    for char in k:
+        hash_value = (hash_value * a + ord(char)) % m
+    return hash_value
 
 def insert(d,key,value):
     index = hashFunction(key,len(d))
     if d[index] is None:
         d[index] = [(key,value)]
     else:
+        if key in d[index][0]:
+            return None
         d[index].append((key,value))
     return d
 
@@ -67,8 +61,119 @@ def isPermutation(s1,s2):
             return False
     return True
 
+##Ejercicio 5
+#O(n) where n is the length of the list
+def checkRepetitions(s):
+    d = [None] * len(s)
+    for char in s:
+        if search(d,char) is None:
+            insert(d,char,char)
+        else:
+            return True
+    return False
+
+##Ejercicio 6
+def postalCode(d,pc):
+    return hashFunction(pc,len(d))
+
+##Ejercicio 7
+
+def countRepetitions(s):
+    d1 = [None] * len(s)
+    d2 = {}
+    for char in s:
+        if search(d1,char) is None:
+            insert(d1,char,1)
+            d2[char] = 1
+        else:
+            d2[char] += 1
+    return str(d2)
+
+##Ejercicio 8
+#O(s + p) donde s es la longitud del primer string y p la longitud del segundo
+def firstOcurrence(s,p):
+    d = [None] * len(s)
+    i = 0
+    
+    for char in s:
+        insert(d,char + str(i),i)
+        i += 1
+
+    for j in range(0,len(d)):
+        if search(d,p[0] + str(j)) is not None:
+            aux = j
+            break
+    if aux is None:
+        return None
+    
+    i = search(d,p[0] + str(aux))
+    j = i
+    for char in p:
+        if search(d,char + str(i)) != i:
+            return None
+        i += 1
+    return j
+
+##Ejercicio 9
+#Complejidad promedio de O(n) donde n es la longitud de t
+def isIncluded(s,t):
+    d = [None] * len(s)
+    for char in t:
+        insert(d,char,'')
+    for char in s:
+        if search(d,char) is None:
+            return False
+    return True
+
+
+##Ejercicio 10
+def hashFunctionOpenAddressing(d,k,m):
+    hash_value = 0
+    k = str(k)
+    a = 17921
+    for char in k:
+        hash_value = (hash_value * a + ord(char)) % m
+    for i in range(0,m):
+        if d[hash_value] is not None:
+            if hash_value < m:
+                hash_value += 1
+            else:
+                hash_value = 0
+        else:
+            break
+    return hash_value + i
+
+def insertOpenAddressing(d,key,value):
+    index = hashFunctionOpenAddressing(d,key,len(d))
+    if d[index] is None:
+        d[index] = [(key,value)]
+    else:
+        if key in d[index][0]:
+            return None
+        d[index].append((key,value))
+    return d
+
+def searchOpenAddressing(d,key):
+    index = hashFunctionOpenAddressing(d,key,len(d))
+    print(index)
+    return d[index]
+
+
+
+d = [None] * 11
+insertOpenAddressing(d,'12',1)
+insertOpenAddressing(d,'13',2)
+insertOpenAddressing(d,'14',3)
+insertOpenAddressing(d,'15',4)
+print(searchOpenAddressing(d,'12'))
+
 
 '''
+if isPermutation('hola','hola'):
+    print('Son permutaciones')
+else:
+    print('No son permutaciones')
+
 d = [None] * 11
 insert(d,'asdfafsdsaa','1')
 insert(d,'holaBuenasTasdaardesComoEstas','2')
