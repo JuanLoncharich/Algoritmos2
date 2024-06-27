dee5722a1e1cc3bc6daccd084e8bda3c0166a9c4
JuanLoncharich <juanandresloncha@gmail.com>
2024-06-27 12:47:18 -0300

    Operations for graphs

--- practicas/tp-grafos/code/graph.py
+++ practicas/tp-grafos/code/graph.py
@@ -1,5 +1,3 @@
-
-    
 def printGraph(g):
     for vertex in g:
         print(vertex,":",g[vertex])
@@ -16,6 +14,8 @@ def createGraph(v,e):
 
     return graph
 
+#Determina si existe un camino desde v1 a v2
+
 def existPath(g,v1,v2):
     visited = {}
     stack = [v1]
@@ -27,13 +27,17 @@ def existPath(g,v1,v2):
             visited[vertex] = True
             stack.extend([neighbor for neighbor in g[vertex] if neighbor not in visited])
     return False
-    
+
+#Determina si el grafo es conexo
+
 def isConnected(g):
     for vertex in g:
         for vertexIns in g:
             if existPath(g,vertex,vertexIns) is False:
                 return False
     return True
+
+#Determina si el grafo es un árbol
 
 def isTree(g):
     if isConnected(g) is False:
@@ -49,6 +53,8 @@ def existCycle(g):
                 return True
     return False
 
+#Determina si el grafo es completo
+
 def isComplete(g):
     if isConnected(g) is False:
         return False
@@ -58,7 +64,32 @@ def isComplete(g):
     return True
 
 #Crea arbol abarcador en base a grafo g
-
+def convertTree(g):
+    if g is None or not isConnected(g):
+        return None
+    if not existCycle(g):
+        return g
+    longestValue = max(g.items(), key=lambda item: len(item[1]))[0]
+    vertexs = [longestValue]
+    edges = []
+    
+    for vertex in g[longestValue]:
+        if vertex not in vertexs:
+            vertexs.append(vertex)
+            edges.append((longestValue, vertex))
+            
+    g1 = createGraph(vertexs, edges)
+    
+    while len(vertexs) < len(g):
+        for newValue in vertexs:
+            for vertex in g[newValue]:
+                if vertex not in vertexs:
+                    vertexs.append(vertex)
+                    edges.append((newValue, vertex))
+        g1 = createGraph(vertexs, edges)
+    
+    return g1
+        
 
 #Cuenta la cantidad de componentes conexas en un grafo
 def countConnections(g):
@@ -68,7 +99,6 @@ def countConnections(g):
     conj = [next(iter(g.items()))[0]]
     contador = 1
     return countConnectionsR(g,conj,contador)
-
 
 def countConnectionsR(g,conj,contador):
     if g is None:
@@ -85,19 +115,75 @@ def countConnectionsR(g,conj,contador):
             return countConnectionsR(g,conj,contador)
     return contador
 
+#BFS(Breadth-First Search)
 
-#def convertToBfsTree(g,v):
+def convertToBfsTree(g,v):
+    if g is None:
+        return None
+    if v not in g:
+        return
+    colors = {}
+    colors[v] = 'gray'
+    bfsTree = {}
+    
+    bfsTree = {vertex: [] for vertex in g}
+    
+    for vertex in g:
+        colors[vertex] = 'white'
+    queue = [v]
+    while len(queue) != 0:
+        ver = queue.pop(0)
+        for vertex in g[ver]:
+            if vertex != ver:
+                if colors[vertex] == 'white':
+                    bfsTree[ver].append(vertex)
+                    bfsTree[vertex].append(ver)
+                    colors[vertex] = 'gray'
+                    queue.append(vertex)
+        colors[ver] = 'black'
+    return bfsTree
 
-#def convertToDfsTree(g,v):
+#DFS(Depth-First Search)
+
+def convertToDfsTree(g,v):
+    if g is None:
+        return None
+    if v not in g:
+        return
+    colors = {}
+    colors[v] = 'gray'
+    dfsTree = {vertex: [] for vertex in g}
+    for vertex in g:
+        colors[vertex] = 'white'
+        
+    for vertex in g[v]:
+        if colors[vertex] == 'white':
+            convertToDfsTreeR(g,vertex,colors,dfsTree)
+    return dfsTree
+
+def convertToDfsTreeR(g,v,colors,dfsTree):
+    colors[v] = 'gray'
+    for vertex in g[v]:
+        if colors[vertex] == 'white':
+            dfsTree[v].append(vertex)
+            convertToDfsTreeR(g,vertex,colors,dfsTree)
+    colors[v] = 'black'
+    
+#Camino más corto(no Dijkstra)    
 
 #def bestRoad(g,v1,v2):
 
+#Determina si el grafo es bipartito
+
 #def isBipartite(g):
 
+#Implementa el algoritmo de PRIM
+
 #def PRIM(g):
+    
+#Implementa el algoritmo de Kruskal
 
 #def KRUSKAL(g):
 
-g = createGraph(['A','B','C','D','E','F','G','H'],[('A', 'B'), ('A', 'C'), ('B', 'C'), ('B', 'D'), ('C', 'D'), ('D', 'E'), ('D', 'F'), ('E', 'F'), ('G', 'H')])
-print(countConnections(g))
-#print(convertToBfsTree(g,'A'))
+g = createGraph(['A', 'B', 'C','D', 'E'], [('A', 'B'), ('A', 'C'), ('B', 'C'), ('E', 'A'),('D','E'),('C','E')])
+print(convertToDfsTree(g,'A'))
